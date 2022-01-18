6bbc7e3152056c6ce3891cbbfbf6fc799dbb7b8e
Winnrman <andre.jschoolman@gmail.com>
2022-01-18 14:46:35 -0500

    added some testing images for buttons but they don't work :(

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,12 @@ module.exports = {
                 exclude: /node_modules/,
             },
             {
-              test: /\.css$/i,
-              use: ['style-loader', 'css-loader'],
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
         ],
     },
